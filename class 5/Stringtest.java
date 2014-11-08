d246c50006b8477f98966ff9320742eb0c22eea1
Juan Carvajal <juanesteban100@gmail.com>
2014-11-08 12:08:41 -0500

    String class notes

---
+++ class 5/Stringtest.java
@@ -0,0 +1,24 @@
+/*
+*
+* Topic - Strings
+*	Questions
+* 	- How it works the contatenation in terms of memory used
+* 	- Wich is the default capacity of the StringBuilder Class (Important) - amount of characters
+* 	- StringBuilder vs Stringbuffer
+* 	- Cuando Java concatena una linea lo pasa a StringBuilder si son mas lineas no lo pasa a StringBuilder
+* 	- 2 StringBuilder no son iguales asi tengan el mismo literal
+*   - El Paquete Java Lang
+*/
+public class Stringtest{
+
+	String str1 = new String();
+	String str2 = new String();
+
+
+	public static void main(){
+		System.out.println("Hola Mundo");
+	}
+
+
+
+}
